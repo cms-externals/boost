272dcd19570717473e4bd7cf025b895069ce5be4
David <David.Lange@cern.ch>
2020-07-18 22:04:09 +0200

    Revert "apply pr 135 from boost datetime"
    This reverts commit 41648e465122184f7a07c40683a13d177dac6e05.

--- boost/date_time/gregorian/greg_weekday.hpp
+++ boost/date_time/gregorian/greg_weekday.hpp
@@ -48,44 +48,11 @@ namespace gregorian {
     BOOST_CXX14_CONSTEXPR value_type as_number() const {return value_;}
     BOOST_CXX14_CONSTEXPR weekday_enum as_enum() const {return static_cast<weekday_enum>(value_);}
 
-      //! Return a 3 digit english string of the day of week (eg: Sun)
-    const char* const as_short_string() const
-    {
-      static const char* const short_weekday_names[]=
-	{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-
-      return short_weekday_names[value_];
-    }
-
-    //! Return a point to a long english string representing day of week
-    const char* const as_long_string() const
-    {
-      static const char* const long_weekday_names[]
-	= {"Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday"};
-
-      return long_weekday_names[value_];
-    }
-
-
+    const char* as_short_string() const;
+    const char* as_long_string()  const;
 #ifndef BOOST_NO_STD_WSTRING
-
-    //! Return a 3 digit english wchar_t string of the day of week (eg: Sun)
-    const wchar_t* as_short_wstring() const
-    {
-      static const wchar_t* const w_short_weekday_names[]={L"Sun", L"Mon", L"Tue",
-							   L"Wed", L"Thu", L"Fri", L"Sat"};
-      return w_short_weekday_names[value_];
-    }
-
-    //! Return a point to a long english wchar_t string representing day of week
-    const wchar_t* as_long_wstring()  const
-    {
-      static const wchar_t* const w_long_weekday_names[]= {L"Sunday",L"Monday",L"Tuesday",
-							   L"Wednesday", L"Thursday",
-							   L"Friday", L"Saturday"};
-      return w_long_weekday_names[value_];
-    }
-
+    const wchar_t* as_short_wstring() const;
+    const wchar_t* as_long_wstring()  const;
 #endif // BOOST_NO_STD_WSTRING
 
 
