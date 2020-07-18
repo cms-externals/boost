f5de52ca545ac845a155e7d6d2cc8e53cd99a997
David <David.Lange@cern.ch>
2020-07-18 14:51:03 +0200

    adding header patch from Vassil (also upstreamed)

--- boost/algorithm/string/detail/case_conv.hpp
+++ boost/algorithm/string/detail/case_conv.hpp
@@ -15,8 +15,10 @@
 #include <locale>
 #include <functional>
 
+#include <boost/iterator/transform_iterator.hpp>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
 #include <boost/type_traits/make_unsigned.hpp>
-
 namespace boost {
     namespace algorithm {
         namespace detail {
