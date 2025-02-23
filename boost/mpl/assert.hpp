5369a24509955816070352d13f77990de52041a3
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-02-23 11:17:44 +0100

    ignore parentheses warning

--- boost/mpl/assert.hpp
+++ boost/mpl/assert.hpp
@@ -184,8 +184,7 @@ template< typename P > struct assert_arg_pred_not
     typedef typename assert_arg_pred_impl<p>::type type;
 };
 
-#if BOOST_WORKAROUND(BOOST_GCC_VERSION, >= 80000)
-#define BOOST_MPL_IGNORE_PARENTHESES_WARNING
+#ifdef BOOST_MPL_IGNORE_PARENTHESES_WARNING
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wparentheses"
 #endif
